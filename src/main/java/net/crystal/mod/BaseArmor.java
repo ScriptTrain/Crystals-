76b5c2e0b83692f71693992ea65355750bb136bd
Scripttrain <scripttrain@gmail.com>
2020-07-11 16:34:48 -0700

    testing github

--- src/main/java/net/crystal/mod/BaseArmor.java
+++ src/main/java/net/crystal/mod/BaseArmor.java
@@ -7,7 +7,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 
 public class BaseArmor extends ArmorItem {
-
+//comment
     public BaseArmor(ArmorMaterial material, EquipmentSlot slot) {
         super(material, slot, new Item.Settings().group(ItemGroup.COMBAT));
     }
